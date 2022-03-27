7e22cccf4c072dec24b32ad211e692b81dcccacc
JasonSo <jasontristanso@gmail.com>
2022-03-28 00:55:05 +0800

    first commit

---
+++ html_ex1/activity1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 1</title>
+</head>
+<body>
+    <table border="1" width="700px">
+        <tr>
+            <td><h2><center>Full Name</center></h2></td>
+            <td><b><center>Jason Tristan K. So</center></b></td>
+            <td><center><img height="150px" width="150px" src="https://www.kolabtree.com/img/empty_profile.gif" alt="pic"></center></td>
+        </tr>
+        <tr>
+            <td><h2><center>Address</center></h2></td>
+            <td colspan="2"><b>Dr. 1 Montegrande Apt. Sudlon Rd. M.D. Echavez Maguikay, Mandaue City, Cebu 6014</b></td>
+        </tr>
+        <tr>
+            <td><h2><center>Age</center></h2></td>
+            <td colspan="2"><center><b>25</b></center></td>
+        </tr>
+        <tr>
+            <td><h2><center>Gender</center></h2></td>
+            <td colspan="2"><b><center>Male</center></b></td>
+        </tr>
+        <tr>
+            <td><h2><center>Educational Background</center></h2></td>
+            <td colspan="2">
+                <table border="1">
+                    <tr>
+                        <td><b>Primary Education</b></td>
+                        <td><b>Cebu Normal University - Integrated Laboratory School 2004-2010</b></td>
+                    </tr>
+                    <tr>
+                        <td><b>Secondary Education</b></td>
+                        <td><b>Cebu Normal University - Integrated Laboratory School 2010-2014</b></td>
+                    </tr>
+                    <tr>
+                        <td><b>Tertiary Education</b></td>
+                        <td><b>University of San Carlos - Technological Center <br>Bachelor of Science in Computer Engineering 2014-2019</b></td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td><h2><center>Work Experience</center></h2></td>
+            <td colspan="2">
+                <table border="1" width="560px">
+                    <tr>
+                        <td colspan="2">
+                            <b>QA Web Application Tester</b>
+                            <br>Forty Degrees Celsius Inc
+                            <br>August 2021-Present
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <b>Fullstack Developer OJT</b>
+                            <br>Increment Technologies Inc
+                            <br>October 2018-April 2019
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ html_ex1/chessboard.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chessboard</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>A</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>B</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>C</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>D</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>E</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>F</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>G</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>H</center></td>
+            <td height="50px" width="50px" bgcolor="brown"></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>8</center></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>8</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>7</center></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>7</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>6</center></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>6</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>5</center></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>5</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>4</center></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>4</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>3</center></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>3</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>2</center></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>2</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"><center>1</center></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="green"></td>
+            <td height="50px" width="50px" bgcolor="#eee"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>1</center></td>
+        </tr>
+        <tr>
+            <td height="50px" width="50px" bgcolor="brown"></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>A</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>B</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>C</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>D</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>E</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>F</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>G</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center>H</center></td>
+            <td height="50px" width="50px" bgcolor="brown"><center></center></td>
+        </tr>
+    </table>
+</body>
+</html>
