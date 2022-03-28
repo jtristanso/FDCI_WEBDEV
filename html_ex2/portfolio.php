1ab1e5641df5b532f84fca4c2045b7474f4a1321
JasonSo <jasontristanso@gmail.com>
2022-03-29 00:46:30 +0800

    kuwang pa

--- html_ex2/portfolio.php
+++ html_ex2/portfolio.php
@@ -11,7 +11,7 @@
     <!-- NAVBAR -->
     <div id="navbar">
         <ul id="navbar-order">
-            <li class="navbar-item"><a href=#>ABOUT</a></li>
+            <li class="navbar-item"><a href=#divCentering1>ABOUT</a></li>
             <li class="navbar-item"><a href=#>EXPERIENCE</a></li>
             <li class="navbar-item"><a href=#>PORTFOLIO</a></li>
             <li class="navbar-item"><a href=#>SKILLS</a></li>
@@ -24,14 +24,41 @@
         <div id="mc-section">
            <h1>JASON TRISTAN K. SO</h1> 
         </div>
-        <div id="about-section">
-            <h1>ABOUT</h1>
+        <div id="divCentering1" class="content-section">
+            <h1>ABOUT JASON</h1>
+            <div class="para">
+                <p>Hi! I'm Jason.</p>
+                <p>A QA Tester who's seeking experience on top of educational foundation from working in the industry.</p>
+                <p>Open to being trained for new skills. Experienced in working with a team</p>
+                <p>A hardworking, goal driven, and commited professional who's always looking for ways to better myself</p>
+            </div>
         </div>
-        <div>
+        <div id="divCentering2" class="content-section">
             <h1>EXPERIENCE</h1>
+            <div class="para">
+                <table border="1" width="100%">
+                    <tr>
+                        <td>
+                            <b>QA Web Application Tester</b>
+                            <br>Forty Degrees Celsius Inc
+                            <br>August 2021-Present
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <b>Fullstack Developer OJT</b>
+                            <br>Increment Technologies Inc
+                            <br>October 2018-April 2019
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div>
+        <div id="divCentering3" class="content-section">
             <h1>PORTFOLIO</h1>
+            <div class="para">
+                
+            </div>
         </div>
         <div></div>
         <div></div>
