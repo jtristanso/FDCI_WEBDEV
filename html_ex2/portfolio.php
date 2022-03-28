a3fad7ba462176d10824eb0f2379b4cdd7b6a3c8
JasonSo <jasontristanso@gmail.com>
2022-03-28 23:50:11 +0800

    stuff

---
+++ html_ex2/portfolio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>JASON SO</title>
+</head>
+<body>
+    <!-- NAVBAR -->
+    <div id="navbar">
+        <ul id="navbar-order">
+            <li class="navbar-item"><a href=#>ABOUT</a></li>
+            <li class="navbar-item"><a href=#>EXPERIENCE</a></li>
+            <li class="navbar-item"><a href=#>PORTFOLIO</a></li>
+            <li class="navbar-item"><a href=#>SKILLS</a></li>
+            <li class="navbar-item"><a href=#>EDUCATION</a></li>
+            <li class="navbar-item"><a   href=#>INTERESTS</a></li>
+        </ul>
+    </div>
+    <!-- CONTENT -->
+    <div id="content-container">
+        <div id="mc-section">
+           <h1>JASON TRISTAN K. SO</h1> 
+        </div>
+        <div id="experience-section">
+            <h1>EXPERIENCE</h1>
+        </div>
+        <div>
+            <h1>PORTFOLIO</h1>
+        </div>
+        <div>
+            <h1>SKILLS</h1>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+</body>
+</html>
