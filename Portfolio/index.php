a6f3fb74aac790bcc7dd52d93d8b51787ece53b2
JasonSo <jasontristanso@gmail.com>
2022-04-03 22:26:29 +0800

    portfolio with bootstrap pero g template nya edit2 kay kapoy from 0

---
+++ Portfolio/index.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Jason So</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/suisei.jpg" rel="icon">
+  <link href="assets/img/suisei.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/suisei.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Jason So</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://github.com/jtristanso" class="github"><i class="bx bxl-github"></i></a>
+          <a href="https://www.facebook.com/KojiBang" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://join.skype.com/invite/xYFPCSdCHU6f" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="https://www.linkedin.com/in/jason-tristan-so-935249189/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+          <li><a href="#interests" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Interests</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Jason So</h1>
+      <p>I'm a <span class="typed" data-typed-items="Tester"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>Hi! I'm Jason. A QA Tester who's seeking experience on top of educational foundation from working in the industry. Open to being trained for new skills. Experienced in working with a team. A hardworking, goal driven, and commited professional who's always looking for ways to better himself. ＼(￣▽￣)／</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/suisei.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>QA Web Application Tester</h3>
+            <p class="fst-italic">
+              Develops and executes test plans and cases to ensure that there are only a few or no bugs released in deployment.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>25 January 1997</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>(+63)915 310 9876</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Mandaue City, Cebu</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>25</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>jasontristanso@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor's in Computer Engineering</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Skills</h2>
+          <p>Rough summary of the skills I've accumulated over the years</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 content" data-aos="fade-up">
+            <ul>
+              <li><h3>Software Skills</h3></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Training Experience in C, C++, PHP, HTML, CSS</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Training Experience in JavaScript and Objective-C</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Experience in Notepad++, Visual Studio Code, and Sublime Text</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Experience in Dev-C++, Xcode, and Arduino IDE</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Knowledgeable in GitHub usage</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Novice in C#, Vue.js, and Laravel</span></li>
+            </ul>
+          </div>
+          <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+            <ul>
+              <li><h3>Technical Skills</h3></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Hardware Troubleshooting</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Hardware Description Language (Verilog)</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Computer Networks and Hardware (CISCO)</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>PCB Design and Fabrication using EagleCAD and Proteus Design Suite</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Microsoft Office and Google Workspace</span></li>
+              <li><i class="bi bi-chevron-right"></i> <span>Basic MatLab programming</span></li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <p>Education and Professional Experience</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>QA Web Application Tester</h4>
+              <h5>August 2021 - Present</h5>
+              <p><em>Forty Degrees Celsius Inc. IT Park, Cebu City </em></p>
+              <ul>
+                <li>Analyzed functional and system specifications then developed and executed test scenarios and cases</li>
+                <li>Tested features developed by the company to minimize bugs</li>
+                <li>Assisted development team in debugging and replicating reported issues</li>
+                <li>Tested features developed by Vietnamese partner company to ensure quality during deployment</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Full Stack Developer OJT</h4>
+              <h5>October 2018 - April 2019</h5>
+              <p><em>Increment Technologies Inc. Mabolo, Cebu City</em></p>
+              <ul>
+                <li>Worked on in-house projects, a classroom management application and an ID manufacturing website</li>
+                <li>Used Vue.js and Laravel as frameworks with PHP, HTML, and JS. XAMPP MySQL for database, and GitHub for version control and collaboration</li>
+                <li>Developed and implemented new features for a market page on both projects</li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Bachelor of Science in Computer Engineering</h4>
+              <h5>June 2014 - December 2019</h5>
+              <p><em>University of San Carlos - Technological Center, Talamban, Cebu City</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Highschool Graduate</h4>
+              <h5>June 2010 - March 2014</h5>
+              <p><em>Cebu Normal University - Integrated Laboratory School, Osmeña Blvd, Cebu City</em></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p>Projects worked on:</p>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-6 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <h3>Native Camp</h3>
+              <img src="assets/img/NC.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="https://app.adjust.com/d5f9eaw" title="App Download"><i class="bx bxs-mobile"></i></a>
+                <a href="https://nativecamp.net/" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <h3>Undergraduate Thesis</h3>
+              <img src="assets/img/thesis.png" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="assets/thesis.pdf" download title="More Details"><i class="bx bxs-download"></i></a>
+                <a href="https://drive.google.com/file/d/12mDGpdIeHhFa4fcis4S5Aar0rMXXS2as/view?usp=sharing" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Interests Section ======= -->
+    <section id="interests" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Interests</h2>
+          <p>Aside from work, I have several hobbies and I enjoy a variety of things, here are some of them:</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+            <div class="icon"><i class="bi bi-controller"></i></div>
+            <h4 class="title">Gaming</h4>
+            <p class="description">I love playing games, especially with my friends. It's a way for me to spend time with them without actually meeting up.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon"><i class="bi bi-book"></i></div>
+            <h4 class="title">Reading</h4>
+            <p class="description">I read a lot, mostly manga and light novels. But I usually read anything that seems interesting to me.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+            <div class="icon"><i class="bi bi-youtube"></i></div>
+            <h4 class="title">Movies/Shows</h4>
+            <p class="description">I love movies. Who doesn't? If you know a good movie/show, suggest it to me please.</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+            <div class="icon"><i class="bi bi-scissors"></i></div>
+            <h4 class="title">Scale Model Building</h4>
+            <p class="description">I also do some plastic model assembly and collection. Mostly Gunpla :D</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+            <div class="icon"><i class="bi bi-card-list"></i></div>
+            <h4 class="title">Collecting</h4>
+            <p class="description">I collect a variety of things. Online skins from games, to playing and trading cards(Yugioh).</p>
+          </div>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon"><i class="bi bi-camera-video"></i></div>
+            <h4 class="title">Watching Streamers</h4>
+            <p class="description">During some down time, I watch streamers on Youtube or Twitch. They're entertaining and sometimes I learn something new.</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Interests Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Feel free to contact me if you have something to inquire about.</p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Dr. 1 Montegrande Apt. Sudlond Rd. M.D. Echavez Maguikay, Mandaue City, Cebu</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>jasontristanso@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call/Text:</h4>
+                <p>+63915 310 9876</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>iPortfolio</span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
