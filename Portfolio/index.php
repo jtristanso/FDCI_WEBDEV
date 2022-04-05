35ded88a81c5e8fe7e989015b591be2a1475c43e
fdc.so <fdc.jasonso@gmail.com>
2022-04-05 20:06:37 +0800

    Gmaps g add para chuy

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -294,7 +294,17 @@
         </div>
 
         <div class="row" data-aos="fade-in">
-
+          <div class="mapouter">
+            <div class="gmap_canvas">
+              <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Mandaue%20City,%20Cebu&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+              <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a>
+              <br>
+              <style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>
+              <a href="https://www.embedgooglemap.net">embed google map without api key</a>
+              <style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+            </div>
+          </div>
+          
           <div class="col-lg d-flex align-items-stretch">
             <div class="info">
               <div class="address">
